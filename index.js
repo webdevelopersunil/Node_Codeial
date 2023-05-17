d589e8810640defa2b38e93433a89155b8672400
webdevelopersunil <he.kingsmen@gmail.com>
2023-05-17 22:22:38 +0530

    Profile Users Routes and Controller

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app   =   express();
 const port  =   8000;
 
+
+// Set up default the template engine ejs
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+
 //use express routes
 app.use('/', require('./routes'));
 app.use('/users', require('./routes/users'));
-
-
-
 
 
 
